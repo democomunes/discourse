01ecc0af27acc8d598d73d28783e6bd03224a923
Antonio Pardo <apardo@alabs.org>
2016-05-29 17:08:12 +0200

    discourse config

--- config/initializers/100-secret_token.rb
+++ config/initializers/100-secret_token.rb
@@ -1,7 +1,7 @@
 # We have had lots of config issues with SECRET_TOKEN to avoid this mess we are moving it to redis
 #  if you feel strongly that it does not belong there use ENV['SECRET_TOKEN']
 #
-token = ENV['SECRET_TOKEN']
+token = "2a262e5a3fa362f6dd3796c793f19ca8b5bd7953767bca3af46726f079cc7fa179c75945f95e3d6c99d4e3eca9b62acad2386848605926cb73127bc27ff24555"
 unless token
   token = $redis.get('SECRET_TOKEN')
   unless token && token.length == 128
